59e7052f899967ba3a379fb1a3ab06b2e4383568
Arpita Lonakadi <104141167+Arpl14@users.noreply.github.com>
2025-04-01 00:34:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -148,13 +148,13 @@ if uploaded_file is not None:
 
 
 
-if selected_user == 'Overall':
+    if selected_user == 'Overall':
     # Generate network graph for group
-    network_fig = stats.network_analysis(df)
-    st.pyplot(network_fig)
+        network_fig = stats.network_analysis(df)
+        st.pyplot(network_fig)
 
         # Display the user with the maximum responses
-        most_active_user, most_active_user_responses = stats.max_responses_user(df)
+     most_active_user, most_active_user_responses = stats.max_responses_user(df)
         st.write(f"Most active user: {most_active_user} with {most_active_user_responses} responses.")
         st.write("The color of the nodes represents the activity level of each user. Darker colors indicate users with higher activity levels, and the node size is based on the number of messages they sent.")
 
