03ae7a5c480e3d5851cc4a88f9de84725f9ede4e
Arpita Lonakadi <104141167+Arpl14@users.noreply.github.com>
2025-04-01 02:03:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import preprocess
 import stats
 import matplotlib.pyplot as plt
 
+
+    st.title("Whatsapp Chat Analyser")
 # Title for the Streamlit app
 st.sidebar.title("WhatsApp Chat Analyzer")
 # File uploader to upload WhatsApp group chat file
